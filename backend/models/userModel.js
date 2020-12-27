235670659312057c886f159daf326c970b4f721f
Piyush55dude <piyushagarwalvo@gmail.com>
2020-12-27 17:24:52 +0530

    Login + Registration Routes Added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -16,6 +16,17 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
   },
   {
     timestamps: true,
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(express.json()); // to accept json data
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
+
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
