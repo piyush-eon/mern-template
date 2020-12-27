a7140d5f991832051851dab6e0c617e5a1245e6e
Piyush55dude <piyushagarwalvo@gmail.com>
2020-12-27 14:04:20 +0530

    Initislized an added user model

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express(); // main
+
+app.use(express.json()); // to accept json data
+
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}..`.yellow
+      .bold
+  )
+);
